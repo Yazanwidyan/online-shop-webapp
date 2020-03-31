fcef0066af6377f651f14d8c78c4afab8d101db2
yazan widyan <yazanwidyan1@gmail.com>
2020-03-31 23:10:28 +0300

    deploying heroku

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const bodyparser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const { User } = require("./models/user");
+const { User } = require("./models/User");
+const { auth } = require("./middleware/auth");
 const config = require("./config/key");
 
 mongoose
@@ -19,13 +20,64 @@ app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 app.use(cookieParser());
 
-app.post("/api/users/register", (req, res) => {
-  const user = new User(req.body);
-
-  user.save((err, userData) => {
-    if (err) return res.json({ success: false, err });
-    return res.status(200).json({ success: true });
+app.get("/api/user/auth", auth, (req, res) => {
+  res.status(200).json({
+    _id: req._id,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role
   });
 });
 
-app.listen(5000);
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true, userData: doc });
+  });
+});
+
+app.post("/api/user/login", (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user)
+      return res.json({
+        loginSuccess: false,
+        message: "Auth failed,email not found"
+      });
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          message: "wrong password"
+        });
+      }
+    });
+    user.generateToken((err, user) => {
+      if (err) return res.status(400).send(err);
+      res
+        .cookie("x_auth", user.token)
+        .status(200)
+        .json({
+          loginSuccess: true
+        });
+    });
+  });
+});
+
+app.get("/api/user/logout", auth, (req, res) => {
+  User.findByIdAndUpdate({ _id: req.user._id }, { token: "" }, (err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true
+    });
+  });
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`server running on ${port}`);
+});
