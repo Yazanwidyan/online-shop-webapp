584d83ac4801baa59d4929fe4a3c373a551bbf2d
yazan widyan <yazanwidyan1@gmail.com>
2020-04-01 03:10:31 +0300

    adding about page

---
+++ client/src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import About from "./about";
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
